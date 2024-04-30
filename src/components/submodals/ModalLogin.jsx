f6d857288b03aa0350e24dc6d65aadb8089eaf37
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-04-30 19:43:11 +0200

    Consumin privateAxios

--- src/components/submodals/ModalLogin.jsx
+++ src/components/submodals/ModalLogin.jsx
@@ -1,6 +1,6 @@
 import { Modal, Button } from "react-bootstrap";
 import { useState } from "react";
-import axios from "../../api/axios";
+import { privateAxios } from "../../api/axios";
 import { useTokenSignal } from "../../hooks/useTokenSignal";
 
 const ModalLogin = ({ show, onHide }) => {
@@ -12,7 +12,7 @@ const ModalLogin = ({ show, onHide }) => {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post("/auth/", {
+      const response = await privateAxios.post("/auth/", {
         email,
         password,
       });
@@ -35,43 +35,52 @@ const ModalLogin = ({ show, onHide }) => {
       <Modal.Body>
         <p>{errMsg}</p>
         <div className="row">
-          <div className="col">
+          <form className="col">
             <label
               htmlFor="email"
-              className="form-label d-flex justify-content-end p-1"
+              className="form-label d-flex justify-content-between p-1"
             >
               Email
               <input
                 type="email"
                 id="email"
-                className="form-control"
+                className="form-control w-75"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </label>
             <label
               htmlFor="password"
-              className="form-label d-flex justify-content-end p-1"
+              className="form-label d-flex justify-content-between p-1"
             >
               Password
               <input
                 type="password"
                 id="password"
-                className="form-control"
+                className="form-control w-75"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </label>
-          </div>
+            <div className="d-flex justify-content-between">
+              <div></div>
+              <Button
+                variant="primary"
+                type="submit"
+                className="w-75"
+                onClick={handleSubmit}
+              >
+                Log In
+              </Button>
+            </div>
+          </form>
         </div>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onHide}>
           Cancel
         </Button>
-        <Button variant="primary" onClick={handleSubmit}>
-          Log In
-        </Button>
+
         <Button variant="primary" onClick={handleProceedToRegister}>
           Register
         </Button>
