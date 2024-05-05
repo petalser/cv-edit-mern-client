011a3d4b48c8412ee37d5550e8b184f07a7124c6
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-05-05 18:54:09 +0200

    useData hook added

--- src/signals/data.js
+++ src/signals/data.js
@@ -2,3 +2,13 @@ import { signal } from "@preact/signals-react";
 import defaultData from "../data/defaultValues.json";
 
 export const signalData = signal(defaultData);
+
+export const useData = () => {
+  const data = signalData.value;
+
+  const setData = (argument) => {
+    signalData.value = argument;
+  };
+
+  return { data, setData };
+};
